36c9daa4dbe18b5376d77eac17cdd5e8b3872164
Alex Johnson <ajohnson2389@gmail.com>
2022-04-29 22:54:14 -0400

    added VRF Coordinator from @chainlink

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -2,8 +2,11 @@
 
 pragma solidity ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
 error Raffe__SendMoreToEnterRaffle();
 error Raffle__RaffleNotOpen();
+error Raffle__UpkeepNotNeeded();
 
 contract Raffle {
     enum RaffleState {
@@ -19,13 +22,32 @@ contract Raffle {
     uint256 public immutable i_interval;
     address payable[] public s_players;
     uint256 public s_lastTimeStamp;
+    VRFCoordinatorV2Interface public immutable i_vrfCoordinator;
+    bytes32 public i_gasLane;
+    uint64 public i_subscriptionId;
+    uint32 public i_callbackGasLimit;
+
+    uint16 public constant REQUEST_CONFIRMATIONS = 3;
+    uint32 public constant NUM_WORDS = 1;
 
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee, uint256 interval) {
+    constructor(
+        uint256 entranceFee, 
+        uint256 interval, 
+        address vrfCoordinatorV2,
+        bytes32 gasLane, // keyhash
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+
+    ) {
         i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() external payable {
@@ -62,7 +84,25 @@ contract Raffle {
         bool isOpen = RaffleState.Open == s_raffleState;
         bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval); // keep track of time
         bool hasBalance = address(this).balance > 0;
-        upkeepNeeded = (timePassed && hasBalance && isOpen);
-        return (upkeepNeeded, "");
+        bool hasPlayers = s_players.length > 0;
+        upkeepNeeded = (timePassed && hasBalance && isOpen && hasPlayers);
+        return (upkeepNeeded, "0x0");
+    }
+
+    function performUpkeep(
+        bytes calldata /*performData */
+    ) external {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) {
+            revert Raffle__UpkeepNotNeeded();
+        }
+        s_raffleState = RaffleState.Calculating;
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
     }
 }
