ae9a9fa10e4385b34eac3d428052d9b8e842fd61
Alex Johnson <ajohnson2389@gmail.com>
2022-04-29 21:32:43 -0400

    added checkUpkeep function for Chainlink Keepers

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -16,12 +16,16 @@ contract Raffle {
     // will never change. immutable is gas efficient
     // prepend i_ to let devs know cheap variable.
     uint256 public immutable i_entranceFee;
+    uint256 public immutable i_interval;
     address payable[] public s_players;
+    uint256 public s_lastTimeStamp;
 
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
     function enterRaffle() external payable {
@@ -41,11 +45,24 @@ contract Raffle {
     // 1 we want this done automatically
     // 2 we want a real random winner
 
-    // Be true after some time interval
+    // 1. Be true after some time interval
     // 2. The lottery to be open
     // 3. The contract has ETH
     // 4. Keepers has LINK
-    function checkUpkeep() {
-
+    function checkUpkeep(
+        bytes memory /* checkData */
+    ) 
+        public 
+        view 
+        returns (
+            bool upkeepNeeded, 
+            bytes memory /* performData */
+        )
+    {
+        bool isOpen = RaffleState.Open == s_raffleState;
+        bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval); // keep track of time
+        bool hasBalance = address(this).balance > 0;
+        upkeepNeeded = (timePassed && hasBalance && isOpen);
+        return (upkeepNeeded, "");
     }
 }
